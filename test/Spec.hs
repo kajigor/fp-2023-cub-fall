9c21edbdebc9aa5563c4da585013bec50e330620
Sergei Sysoev <Sergey.Sysoev@jetbrains.com>
2023-12-06 12:01:07 +0000

    Tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,35 @@
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import Test.Tasty
+import Test.Tasty.Hedgehog
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  defaultMain (testGroup "All Tests"
+                [ testGroup "RegexProps" props
+                ])
+
+props :: [TestTree]
+props =
+  [ testProperty "Union `a` `a` = `a`" prop_unionAA
+  , testProperty "Union `a` `b` = Union `b` `a`" prop_unionABBA
+  ]
+
+genChar = Gen.enum 'A' 'Z'
+
+genString = Gen.list (Range.linear 0 2) genChar
+
+prop_unionAA :: Property
+prop_unionAA = property $ do
+    string1 <- forAll genString
+    string2 <- forAll genString
+    match string1 (string string2 `union` string string2) === match string1 (string string2)
+
+prop_unionABBA :: Property
+prop_unionABBA = property $ do
+    string1 <- forAll genString
+    string2 <- forAll genString
+    string3 <- forAll genString
+    match string1 (string string2 `union` string string3) === match string1 (string string3 `union` string string2)
